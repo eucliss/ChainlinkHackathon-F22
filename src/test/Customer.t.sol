02c925de03923a9915257c3f1a4860ae7606128b
miller <millerkinlin@gmail.com>
2022-10-16 15:59:15 -0400

    modules

---
+++ src/test/Customer.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "./utils/Helpers.sol";
+
+
+contract CustomerTest is Helpers {
+
+
+}
+
---
+++ src/utils/Customer.sol
@@ -0,0 +1,9 @@
+
+pragma solidity ^0.8.14;
+
+
+contract Customer is Helpers {
+
+
+}
+
