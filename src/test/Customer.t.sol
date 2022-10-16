dd188d31b2894d647c70964918bdbc3ee936b802
miller <millerkinlin@gmail.com>
2022-10-16 16:16:31 -0400

    modules

--- src/test/Customer.t.sol
+++ src/test/Customer.t.sol
@@ -3,10 +3,15 @@
 pragma solidity ^0.8.14;
 
 import "./utils/Helpers.sol";
+import {ICustomer, Customer} from "../utils/Customer.sol";
 
 
 contract CustomerTest is Helpers {
 
+
+    function setUp() public {
+
+    }
 
 }
 
--- src/utils/Customer.sol
+++ src/utils/Customer.sol
@@ -1,8 +1,57 @@
 
 pragma solidity ^0.8.14;
 
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract Customer is Helpers {
+interface ICustomer {
+
+    function deposit() external payable;
+
+    function bill() external;
+
+}
+
+
+contract Customer is ICustomer, Initializable {
+
+    event DepositSuccessful(address sender, uint256 amount);
+
+    address public immutable coordinator;
+    address public owner;
+    uint256 balance = 0;
+
+    function initialize(
+        address _owner;
+        address _coordinator;
+    ) initializer public {
+        owner = _owner;
+        coordinator = _coordinator;
+    }
+
+    receive() {
+        require(msg.value > 0, "NO ETH SENT")
+        deposit();
+    }
+
+
+    function deposit() external override payable {
+        // Maybe want to expand to other ERCs?
+        balance += msg.value;
+        emit DepositSuccessful(msg.sender, msg.value);
+    }
+
+    function bill(uint256 amount) external override returns(bool success) {
+        require(msg.sender == coordinator, "NOT COORDINATOR");
+        if(amount > balance) {
+            return false;
+        } else {
+            (success, )address(coordinator).call{value: amount}("");
+            if(success) {
+                balance -= amount;
+            }
+        }
+    }
+
 
 
 }
