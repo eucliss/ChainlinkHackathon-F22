fc0da15b17f17d0f83722dabb8d245d95f98aa6e
miller <millerkinlin@gmail.com>
2022-10-31 16:13:45 -0400

    So I got a little side tracked ...

--- CurrentSDK/coordinator.py
+++ CurrentSDK/coordinator.py
@@ -7,6 +7,8 @@ from db import MongoDB
 from connector import Connector
 from decimal import Decimal
 from customerStore import CustomerStore
+from assetStore import AssetStore
+from userStore import UserStore
 
 
 
@@ -35,50 +37,67 @@ class Coordinator():
             self, 
             connector:Connector=None, 
             database='CurrentSDK', 
-            collection='Customers'
+            customerCollection='Customers',
+            assetCollection='Assets',
+            userCollection='Users'
         ):
         """Initialize for the coordinator."""
         if connector == None:
             connector = Connector(COORDADDRESS)
+        self.connector = connector
         self.coord = connector.contract
         self.w3 = connector.w3
-        self.customerStore = CustomerStore(database=database, collection=collection)
+        self.customerStore = CustomerStore(database=database, collection=customerCollection)
+        self.assetStore = AssetStore(database=database, collection=assetCollection)
+        self.userStore = UserStore(database=database, collection=userCollection)
 
         # DB
         self.client = MongoDB()
         self.databaseName = database
-        self.collectionName = collection
+        self.assetCollection = assetCollection
+        self.userCollection = userCollection
+        self.customerCollection = customerCollection
         self.db = self.client.setDatabase(database)
-        self.collection = self.client.setCollection(collection)
+        self.collection = self.client.setCollection(customerCollection)
         
     # Register as a customer
 
     def registerCustomer(self):
         """
+        $$ CONTRACT INTERACTION $$
+
+        Calls the contract and registers a new customer
+
+        TODO:
+        Return the customer Identifier and other userful info to fix shit code
+
         Returns a dictionary:
         {
             'customer': address, new created contract
             'controller': address, controller of the contract
         }
         """
-
-        # Create a new customer invoice address
+        # Create a new customer invoice address on the contract, send 0.1 ether
         value = self.w3.toWei(Decimal(0.1), 'ether')
         tx_hash = self.coord.functions.registerCustomer(
             CONTROLLER
         ).transact({'from': REGISTRAR, 'value': value})
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
-        # Need to log this into the mongoDB now
-        self.customerStore.addNewCustomer(tx_receipt.logs[0].address)
+        # Log the data in the mongoDB
+        customerId, success, _ = self.customerStore.addNewCustomer(tx_receipt.logs[0].address)
 
+        # Return the parsed event object from the contract
         event = self.coord.events.CustomerRegistered().processReceipt(tx_receipt)
         event = event[0]['args']
-
         return event
 
     def registerAssets(self, customerInvoice, assetAddresses, assetItemTypes):
         """
+        $$ CONTRACT INTERACTION $$
+
+        Takes assets and registers them to a customerInvoice address location
+
         Returns a dictionary:
         {
             'customer': address, new created contract
@@ -86,8 +105,23 @@ class Coordinator():
             'updatedContracts': address[] updated list of contracts 
         }
         """
+        # Check for existing records for the invoice address
+        recs = self.customerStore.client.getRecord(
+            {
+                'invoiceAddress': customerInvoice
+            }, 
+            db=self.databaseName, 
+            collection=self.customerCollection
+        )
 
-        # Create a new customer invoice address
+        # if the invoice record does not exist, not a valid customer
+        if len(recs) == 0:
+            return 0, False, "Customer invoice not registered"
+        
+        customerIdentifier = recs[0]['customerIdentifier']
+
+        # Register those assets on chain for the customer
+        # Asset controller is the CONTROLLER address
         tx_hash = self.coord.functions.registerAssets(
             CONTROLLER,
             customerInvoice,
@@ -96,43 +130,96 @@ class Coordinator():
         ).transact({'from': CONTROLLER})
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
+        # Parse the event object for the return later
         event = self.coord.events.AddedAssetsToCustomer().processReceipt(tx_receipt)
         event = event[0]['args']
-        # Need to log this into the mongoDB now
+
+        # Log the addresses into the asset store
+        res = self.assetStore.addAssets(customerIdentifier, assetAddresses, assetItemTypes)
 
         return event
 
-    def mintAssets(self, packages, recipients):
+    def mintAssets(self, packages, recipientUsernames):
         """
+        $$ CONTRACT INTERACTION $$
+
+        Mint a set of assets to recipients in the DB.
+        Recipients must be registered, using their username for confirmation
+
         Returns a dictionary:
         {
             'packages': PackageItem[] packages minted to customers
             'recipients': address[] addresses the items were minted to
         }
         """
+        # Check the DB for each of the users in the input
+        # Add their addresses and identifiers to a list for use later
+        userAddresses = []
+        userIdentifiers = []
+        for user in recipientUsernames:
+            recs = self.userStore.client.getRecord(
+                {
+                    'username': user
+                }, 
+                db=self.databaseName, 
+                collection=self.userCollection
+            )
+            if len(recs) == 0:
+                return 0, False, "User not registered"
+            userAddresses.append(recs[0]['address'])
+            userIdentifiers.append(recs[0]['userIdentifier'])
+        
+        # Check the DB for assets, add IDs to a list for later
+        assetIdentifiers = []
+        for item in packages:
+            recs = self.assetStore.client.getRecord(
+                {
+                    'assetAddress': item['token']
+                }, 
+                db=self.databaseName, 
+                collection=self.assetCollection
+            )
+
+            if len(recs) == 0:
+                return 0, False, "Asset not registered"
+            assetIdentifiers.append(recs[0]['assetIdentifier'])
+
+        # Contract call: Mint the assets to the users
         tx_hash = self.coord.functions.mintAssets(
             packages,
-            recipients
+            userAddresses
         ).transact({'from': CONTROLLER})
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+
+        # Parse the event back for confirmation
         event = self.coord.events.MintedAssets().processReceipt(tx_receipt)
         event = event[0]['args']
 
-        # Need to log this into the mongoDB now
+        # Log user updates into the DB now
+        for i  in range(0, len(packages)):
+            obj = {
+                'assetIdentifier': assetIdentifiers[i],
+                'amount': packages[i]['amount'],
+                'id': packages[i]['identifier']
+            }
+            self.userStore.addAssetItemToUser(userIdentifiers[i], obj, self.assetStore)
 
         return event
 
     # Register with assets
     def registerWithAssets(self, assetAddresses, assetItemTypes):
         """
+        $$ CONTRACT INTERACTION $$
+
+        Creates a new customer and also registers assets to their invoice
+
         Returns a dictionary:
         {
-            'customer': address, new created contract
             'additionalContracts': address[] new contracts added
             'updatedContracts': address[] updated list of contracts 
         }
         """
-                # Create a new customer invoice address
+        # Register a new customer with the assets
         value = self.w3.toWei(Decimal(0.1), 'ether')
         tx_hash = self.coord.functions.registerWithAssets(
             CONTROLLER,
@@ -141,10 +228,14 @@ class Coordinator():
         ).transact({'from': CONTROLLER, 'value': value})
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
+        # Parse the event for details
         event = self.coord.events.AddedAssetsToCustomer().processReceipt(tx_receipt)
         event = event[0]['args']
-        # Need to log this into the mongoDB now
-
+        
+        # Add new customer to customer store
+        customerId, success, _ = self.customerStore.addNewCustomer(event['customer'])
+        # Add assets to that customer
+        res = self.assetStore.addAssets(customerId, assetAddresses, assetItemTypes)
         return event
 
 
